218a521c66b9a5034a04d089e4424155b2852ace
ClassCat <support@classcat.com>
2015-05-08 21:09:09 +0900

    Create/Add config.inc.php

---
+++ assets/config.inc.php
@@ -0,0 +1,21 @@
+<?php
+// $config['db_dsnw'] = 'mysql://MYSQL_USER:MYSQL_PASSWORD@MYSQL_HOST:MYSQL_PORT/MYSQL_DB';
+$config['db_dsnw'] = 'mysql://webmail:ClassCatWebmail:@mysql/webmail';
+// $config['default_host'] = 'tls://MAIL_HOST';
+// $config['default_host'] = NULL;
+// $config['smtp_server'] = 'tls://MAIL_HOST';
+$config['smtp_port'] = 25; 
+// $config['smtp_user'] = '%u';
+// $config['smtp_pass'] = '%p';
+$config['support_url'] = '';
+$config['des_key'] = 'ROUNDCUBE_RANDOM';
+// $config['des_key'] = 'S-w?cLQE42DY+yc2hRV&D!7U';
+$config['plugins'] = array();
+$config['language'] = 'en_US';
+
+// $config['spellcheck_engine'] = 'atd';
+// $config['mail_pagesize'] = 250;
+// $config['addressbook_pagesize'] = 250;
+// $config['htmleditor'] = 2;
+// $config['preview_pane'] = true;
+?>
